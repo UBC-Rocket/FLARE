827b454cef2eb982d570818813e98489d886e9e5
a-duen <anthony.duen@gmail.com>
2018-11-09 01:15:54 -0800

    Barometer state machine update

--- avionics/statemachine2.cpp
+++ avionics/statemachine2.cpp
@@ -1,7 +1,7 @@
 /*State Machine Source*/
 
 /*Includes------------------------------------------------------------*/
-#include "statemachine.h"
+#include "statemachine.h2"
 
 #include <Arduino.h>
 #include <SD.h>
@@ -9,21 +9,83 @@
 /*Variables------------------------------------------------------------*/
 File statelog;
 FlightStates state = STANDBY;
-volatile bool launch_interrupt_flag = false;
-
 
 /*Functions------------------------------------------------------------*/
 
-void launchInterrupt()
-{
-    launch_interrupt_flag = true;
-}
-
-void switchState(FlightStates new_state)
-{
+void switchState(FlightStates new_state){
     state = new_state;
 }
 
-void stateMachine(float abs_accel, float altitude, float delta_altitude)
-{
+void stateMachine(float altitude, float delta_altitude) { 
+    static int coast_count, main_count, land_count, apogee_count = 0;
+
+    switch (state) {
+        case STANDBY:
+            if (altitude > (threshold + 200)) {
+                switchState(POWERED_ASCENT);
+            }
+            break;
+
+        case ARMED:
+            if ((abs_accel > LAUNCH_THRESHOLD) || launch_interrupt_flag) {
+                switchState(POWERED_ASCENT);
+            }
+            break;
+
+        case ASCENT:
+            if (abs_accel < COAST_THRESHOLD) {
+                coast_count++;
+                if (coast_count >= COAST_CHECKS) {
+                    switchState(APOGEE_DETECT);
+                    coast_count = 0;
+                }
+            } else {
+                coast_count = 0;
+            }
+            break;
+
+        case MACH_LOCK:
+            if (delta_altitude < 0) {
+                apogee_count ++;
+                if (apogee_count >= APOGEE_CHECKS) {
+                    //deploy drogue and payload
+                    //delay to avoid pressure spikes
+                    switchState(INITIAL_DESCENT);
+                    apogee_count = 0;
+                }
+            } else {
+                apogee_count = 0;
+            }
+            break;
+
+        case INITIAL_DESCENT:
+            if (altitude < FINAL_DESCENT_THRESHOLD) {
+                main_count ++;
+                if (main_count >= MAIN_CHECKS) {
+                    //deploy main
+                    switchState(FINAL_DESCENT);
+                }
+            } else {
+                main_count = 0;
+            }
+            break;
+
+        case FINAL_DESCENT:
+            if (abs_accel > LANDING_THRESHOLD_LOW && abs_accel < LANDING_THRESHOLD_HIGH && altitude < LAND_HEIGHT_THRESHOLD) {
+                land_count++;
+                if (land_count >= LAND_CHECKS) {
+                    //turn off sensors except GPS
+                    switchState(LANDED);
+                }
+            } else {
+                land_count = 0;
+            }
+            break;
+
+        case LANDED:
+            break;
+
+        default:
+            break;
+    }
 }
---
+++ avionics/statemachine2.h
@@ -0,0 +1,41 @@
+/*State Machine Header*/
+#ifndef STATEMACHINE_H2
+#define STATEMACHINE_H2
+
+/*Includes------------------------------------------------------------*/
+
+/*Constants------------------------------------------------------------*/
+#define MIN_CHECKS 5
+
+#define LAUNCH_THRESHOLD 12
+#define LAUNCH_INTERRUPT_PIN 0
+
+#define COAST_THRESHOLD 5
+#define FINAL_DESCENT_THRESHOLD 200 //meters
+#define LAND_HEIGHT_THRESHOLD 150 //meters
+#define LANDING_THRESHOLD_HIGH 1.06
+#define LANDING_THRESHOLD_LOW 0.94
+#define LAND_CHECKS     20
+#define APOGEE_CHECKS   5
+#define COAST_CHECKS    10
+#define MAIN_CHECKS     10
+
+#define BASELINE_PRESSURE 1012 //TODO: calibrate
+
+/*Variables------------------------------------------------------------*/
+enum FlightStates {
+  STANDBY,
+  ARMED,
+  ASCENT,
+  MACH_LOCK,
+  INITIAL_DESCENT,
+  FINAL_DESCENT,
+  LANDED
+};
+
+/*Functions------------------------------------------------------------*/
+void launchInterrupt();
+void switchState(FlightStates new_state);
+void stateMachine(float altitude, float delta_altitude);
+
+#endif
