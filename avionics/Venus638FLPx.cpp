fb10b263200be84a8b7bfca3abbdf40a5562d1b9
Matt541 <mattpeterson145@gmail.com>
2018-11-18 15:09:19 -0800

    Minor style changes and logic fixes

--- avionics/Venus638FLPx.cpp
+++ avionics/Venus638FLPx.cpp
@@ -13,46 +13,41 @@
 bool getGPSData(char* sentence)
 {
     bool status = false;
-	bool dolla$ = false; 
+	bool dolla$ = false;
     int i = 0;
-    //char field[20];
-    elapsedMillis timeout;
 	char c = 'a';
-	
+    elapsedMillis timeout;
+
     if (SerialGPS.available()) {
-		//gps sentence is 70 characters long, buffer can only hold 64 seemingly, only need upto ~40 for altitude 
-		while (SerialGPS.available() && timeout < GPS_TIMEOUT && i < 60) {
+		//gps sentence is 70 characters long, buffer can only hold 64 seemingly, only need up to ~40 for altitude
+		while (SerialGPS.available() && timeout < GPS_TIMEOUT && i < SENTENCE_SIZE) {
 			c = SerialGPS.read();
 
-			/*Uncomment for testing
-			Serial.print("the charcter is (drumrole): ");
-			Serial.println(c);
-			Serial.print("the position is: ");
-			Serial.println(i);
-			*/
+			//uncomment for testing
+			// Serial.print("the charcter is (drumrole): ");
+			// Serial.println(c);
+			// Serial.print("the position is: ");
+			// Serial.println(i);
 
 			//check to see if the first thing in the buffer is the start of the gps sentence
 			if (c == '$') {
-				sentence[i] = c; 
-				//can move on in reading sentence
+				//can move on to reading the sentence
 				dolla$ = true;
+				sentence[i] = c;
 				i++;
-				
 			}
-			//finishing reading the sentence once a $ is read
+			//continue reading the sentence once a $ is read
 			if (dolla$) {
 				sentence[i] = c;
 				i++;
-				
 			}
 		}
     }
 	//if all 60 characters are read we have a succesfull gps read
 	//The G in position 4 is a unique character to $GPGGA
-	if (i == 60 && sentence[4]=='G') { 
+	if (i == SENTENCE_SIZE && sentence[4]=='G') {
 		status = true;
 	}
-	sentence[i] = '\0';
 
     return status;
 }
@@ -62,8 +57,8 @@ void getGPSField(char* sentence, char* buffer, int index)
     int sentencePos = 0;
     int fieldPos = 0;
     int commaCount = 0;
-	//only 60 characters in the sentence 
-    while (sentencePos < 61) {
+	//only 60 characters in the sentence
+    while (sentencePos <= SENTENCE_SIZE) {
         if (sentence[sentencePos] == ',') {
             commaCount++;
             sentencePos++;
@@ -71,65 +66,11 @@ void getGPSField(char* sentence, char* buffer, int index)
         if (commaCount == index) {
             buffer[fieldPos] = sentence[sentencePos];
             fieldPos++;
-            //make sure it doesn't overwrite buffer
-            if(fieldPos > 20){
+            //make sure it doesn't overflow the buffer
+            if (fieldPos >= GPS_FIELD_LENGTH) {
             	break;
             }
         }
         sentencePos++;
     }
-    buffer[fieldPos] = '\0';
 }
-
-
-
-/* old functions
-
-bool getGPSData(char* sentence)
-{
-bool status = false;
-int i = 0;
-char field[20];
-elapsedMillis timeout;
-
-if (SerialGPS.available()) {
-while (SerialGPS.available() && (status == false) && (timeout < GPS_TIMEOUT)) {
-char ch = SerialGPS.read();
-if (ch != '\n' && i < SENTENCE_SIZE) {
-sentence[i] = ch;
-i++;
-} else {
-sentence[i] = '\0';
-i = 0;
-getGPSField(sentence, field, 0);
-if (strcmp(field, "$GPGGA") == 0) {
-status = true;
-}
-}
-}
-}
-
-return status;
-}
-
-void getGPSField(char* sentence, char* buffer, int index)
-{
-int sentencePos = 0;
-int fieldPos = 0;
-int commaCount = 0;
-
-while (sentencePos < SENTENCE_SIZE) {
-if (sentence[sentencePos] == ',') {
-commaCount++;
-sentencePos++;
-}
-if (commaCount == index) {
-buffer[fieldPos] = sentence[sentencePos];
-fieldPos++;
-}
-sentencePos++;
-}
-buffer[fieldPos] = '\0';
-}
-
-*/
