198df8157c89de38250997603d82748f5777f188
allans21 <samallan22@gmail.com>
2019-01-16 15:18:07 -0800

    Fixed compiler error in h file

--- avionics/sensors.h
+++ avionics/sensors.h
@@ -62,6 +62,6 @@ float barSensorInit(void);
 void pollSensors(unsigned long*, float*, float*, float*, float*, float*);
 void logData(unsigned long *, float*, float*, float*, float*, float*, FlightStates, float, float);
 void addToPressureSet(float*, float);
-void
+
 
 #endif
