c4fa19edf5f553efd54b10f973757c6e273f0b15
a-duen <anthony.duen@gmail.com>
2019-04-22 12:25:32 -0700

    Added ematch capability for drogue release

--- avionics/gpio.h
+++ avionics/gpio.h
@@ -10,19 +10,25 @@
 
 //Double, triple check pin assignments
 #define IGNITOR_PIN 16
+#define DROGUE_IGNITOR_PIN 30
+
 #define SERVO_PIN 2
 #define MELODY_PIN 3
+
 #define CONTINUITY_CHECK_PIN 4
 #define CONTINUITY_CHECK_ADC 14
+#define DROGUE_CONTINUITY_CHECK_PIN 9
+#define DROGUE_CONTINUITY_CHECK_ADC 19
+
 #define POWER_STATUS_LED 20
 #define FLIGHT_LED   21
 
 #define INIT_SERVO_POS  60
 #define FINAL_SERVO_POS 160
 
-#define IGNITOR_DELAY 12    // in milliseconds
+#define IGNITOR_DELAY 14    // in milliseconds
 #define SERVO_DELAY 1500    // in miliseconds
-#define CONTINUITY_CHECK_DELAY 35 // in microseconds!
+#define CONTINUITY_CHECK_DELAY 45 // in microseconds!
 
 // tested at 11.1V with a 2.2k/470 divider (606)
 #define DISCONTINUOUS_THRESHOLD 500 // threshold for a discontinuous ematch -> read voltage / 3.3 * 1023
--- avionics/sensors.h
+++ avionics/sensors.h
@@ -13,6 +13,8 @@
 /*Constants------------------------------------------------------------*/
 // #define NOSECONE //enable or disable nosecone-avionics specific functionality
 #define BODY //enable or disable body-avionics specific functionality
+// #define SERVO //if drogue release utilizes CO2 canister
+#define POW //if drogue release utilizes black powder charge
 // #define TESTING //enable or disable debug output
 // #define GROUND_TEST
 
