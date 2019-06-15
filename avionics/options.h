cf1d5e0c93079d04cc48391da9800ba8699ab229
Jesse Li <jesse.li2002@gmail.com>
2019-06-14 21:40:09 -0700

    Cameras off

--- avionics/options.h
+++ avionics/options.h
@@ -2,8 +2,8 @@
 #define OPTIONS_H
 
 /*Constants------------------------------------------------------------*/
-// #define NOSECONE //enable or disable nosecone-avionics specific functionality
-#define BODY //enable or disable body-avionics specific functionality
+#define NOSECONE //enable or disable nosecone-avionics specific functionality
+// #define BODY //enable or disable body-avionics specific functionality
 // #define SERVO //if drogue release utilizes CO2 canister
 #define POW //if drogue release utilizes black powder charge
 // #define TESTING //enable or disable debug output
