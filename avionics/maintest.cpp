702dfb377099c10a461b85c161faf2b5de599b1e
Quinn F <gqferbs@gmail.com>
2019-09-29 16:16:08 -0700

    added come comments

--- avionics/maintest.cpp
+++ avionics/maintest.cpp
@@ -1,12 +1,16 @@
 //
 // Created by Quinn Ferbers on 9/29/2019.
+// This file currently only runs an example test
 //
 
 #include <gtest/gtest.h>
 
+//this empty class can be used to construct data and pre-reqs for certain tests
 class MainTest : public ::testing::Test {
 };
 
+//an example of a very simple test case. A function could be run inside the
+//assert instead
 TEST(MainTest, exampleTest) {
     ASSERT_TRUE(1);
 }
