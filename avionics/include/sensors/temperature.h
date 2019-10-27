a917250534322ade3aa25052efc80d58f4be5ad3
Harry <honeytung@gmail.com>
2019-10-27 13:16:31 -0700

    Initial temperature sensor class design

---
+++ avionics/include/sensors/temperature.h
@@ -0,0 +1,27 @@
+#ifndef TEMPERATURE_H
+#define TEMPERATURE_H
+
+/**
+  * Temperature Sensor Class
+  */
+
+/*Includes------------------------------------------------------------*/
+#include "sensors-interface.h"
+#include "SparkFunTMP102.h"              // Temperature
+
+/*Constants------------------------------------------------------------*/
+#define TEMP_SENSOR_ADDRESS         0x48
+#define TEMPERATURE_STATUS_POSITION 4
+#define TEMPERATURE_DATA_ARRAY_SIZE 1
+
+class Temperature : public ISensor {
+public:
+SensorStatus initSensor();
+SensorStatus readData(float* data);
+
+private:
+TMP102* temp_sensor;
+float temp;
+};
+
+#endif
---
+++ avionics/src/sensors/temperature.cpp
@@ -0,0 +1,25 @@
+/*Includes------------------------------------------------------------*/
+#include "sensors/temperature.h"
+
+SensorStatus Temperature::initSensor() {
+    temp_sensor = new TMP102(TEMP_SENSOR_ADDRESS);
+
+    /*init temp sensor*/
+    #ifdef TESTING
+        SerialUSB.println("Initializing temperature sensor");
+    #endif
+    temp_sensor->begin();
+
+    return NOMINAL;
+}
+
+SensorStatus Temperature::readData(float* data) {
+    #ifdef TESTING
+        SerialUSB.println("Polling temperature sensor");
+    #endif
+
+    temp = temp_sensor->readTempC();
+    data[0] = temp;
+
+    return NOMINAL;
+}
