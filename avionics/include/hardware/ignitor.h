40f4d3f2e2db1bb36f9a1deca8402f83c422cedc
Quinn F <gqferbs@gmail.com>
2019-11-10 15:05:52 -0800

    added ignitor hardware class

---
+++ avionics/include/hardware/ignitor.h
@@ -0,0 +1,44 @@
+#ifndef IGNITOR_H
+#define IGNITOR_H
+
+/**
+  * Ignitor Class
+  */
+
+/*Includes------------------------------------------------------------*/
+#include "hw-interface.h"
+
+/*Constants------------------------------------------------------------*/
+#define IGNITOR_DELAY 25 // in milliseconds
+#define CONTINUITY_CHECK_DELAY 50 // in microseconds
+//tested at 11.1V with a 2.2k/470 divider (606) or 1.3k/330 (642)
+//  or 1.33k/330 (629)
+// threshold for a discontinuous ematch -> read voltage / 3.3 * 1023
+#define DISCONTINUOUS_THRESHOLD 650
+
+class Ignitor : public IHardware {
+    public:
+    /**
+     * @brief ignitor constructor
+     * @param ignitePin the pin for the ignitor
+     * @param continuityPin the pin for checking continuity
+     * @param continuityADCPin the pin for the ADC for checking continuity
+     */
+    Ignitor(uint8_t ignitePin, uint8_t continuityPin,
+        uint8_t continuityADCPin) {
+
+        Ignitor::ignitePin = ignitePin;
+        Ignitor::continuityPin = continuityPin;
+        Ignitor::continuityADCPin = continuityADCPin;
+    }
+
+    void init();
+    void activate();
+
+    private:
+    uint8_t ignitePin;
+    uint8_t continuityPin;
+    uint8_t continuityADCPin;
+};
+
+#endif
