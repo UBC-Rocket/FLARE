bef95c5bbe84706906d6ca2dd59c46e884b13009
Jesse Li <jesse.li2002@gmail.com>
2019-12-30 17:45:10 -0800

    Removed declaration that was already in base

--- avionics/common/include/radio.h
+++ avionics/common/include/radio.h
@@ -56,12 +56,6 @@ public:
     }
 
     /**
-     * Add a subpacket to the queue to be sent. Note that this is a rather low-level utility; there should also be a helper method for any given subpacket that will build up the specific format this needs that you should use instead.
-     * @param dat A SubPktPtr (refer to typedef) containing the data.
-     */
-    void addSubpacket(SubPktPtr dat);
-
-    /**
      * @brief Meat of the action - listens for any incoming packets, then transmits data and performs rocket actions as necessary.
      */
     void listenAndAct();
