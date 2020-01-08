383db2da4e3de645718e255788b8c329f8a29d1c
Jesse Li <jesse.li2002@gmail.com>
2020-01-08 14:43:38 -0800

    Placeholder utility gpios

---
+++ avionics/envs/x86/lib/Utility/gpio.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#include "Utility/gpio.h"
+
+namespace Utility{
+
+void pinMode(uint8_t pin, PinMode mode){
+    //TODO - Maybe report this somehow?
+}
+
+void digitalWrite(uint8_t pin, PinDigital val){
+    //TODO - Maybe report this somehow?
+}
+
+int analogRead(uint8_t pin){
+    //TODO - Maybe report this somehow?
+}
+
+} //namespace Utility
