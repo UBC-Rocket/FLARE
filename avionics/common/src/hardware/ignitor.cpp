95fa265200e937843196ef041636d38473fa3714
Harry <honeytung@gmail.com>
2020-01-12 12:35:39 -0800

    Added isWorking check

--- avionics/common/src/hardware/ignitor.cpp
+++ avionics/common/src/hardware/ignitor.cpp
@@ -30,6 +30,14 @@ void Ignitor::init() {
     }
 }
 
+bool Ignitor::isWorking() {
+    if (status == HardwareStatus::FAILURE) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
 void Ignitor::activate() {
     Hal::digitalWrite(ignitePin, Hal::PinDigital::HIGH);
     Hal::sleep_ms(IGNITOR_DELAY);
