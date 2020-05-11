ad6e0ffb5eff45a81e43684b678075c404179bf6
Jesse Li <jesse.li2002@gmail.com>
2020-05-10 22:56:07 -0700

    everything is borken

--- avionics/common/include/radio_queue.h
+++ avionics/common/include/radio_queue.h
@@ -1,7 +1,7 @@
 #ifndef RADIO_QUEUE_H_8E9F7D041E4A491889DEA1A194DD56A0
 #define RADIO_QUEUE_H_8E9F7D041E4A491889DEA1A194DD56A0
 
-#include <deque> // for std::deque
+#include <deque>  // for std::deque
 #include <memory> // for std::unique_ptr
 #include <vector> // for std::vector
 
@@ -12,51 +12,60 @@ constexpr unsigned short RADIO_MAX_SUBPACKET_SIZE = 255;
 typedef std::unique_ptr<std::vector<uint8_t>> SubPktPtr;
 
 class RadioQueue {
-public:
+  public:
     RadioQueue(unsigned short const MAX_BYTES) : M_MAX_BYTES(MAX_BYTES) {}
 
     /**
-     * @brief Push a subpacket to the queue. If the pointer is a null pointer or the size is too large, it will silently fail.
+     * @brief Push a subpacket to the queue. If the pointer is a null pointer or
+     * the size is too large, it will silently fail.
      * @param subpacket SubPktPtr to data.
      * */
     void push(SubPktPtr subpacket);
 
     /**
-     * @brief Returns the next SubPktPtr in the queue (i.e. the oldest element), popping that element. If SubPktPtr is empty, behaviour is undefined.
+     * @brief Returns the next SubPktPtr in the queue (i.e. the oldest element),
+     * popping that element. If SubPktPtr is empty, behaviour is undefined.
      */
     SubPktPtr popFront();
 
     /**
      * @brief Test whether container is empty.
      */
-    bool empty() const {return m_subpacket_q.empty();}
+    bool empty() const { return m_subpacket_q.empty(); }
 
     /**
-     * @brief Get number of bytes of subpacket data queued up. Note that this is not the same as the number of subpackets that are queued.
+     * @brief Get number of bytes of subpacket data queued up. Note that this is
+     * not the same as the number of subpackets that are queued.
      * @return Number of bytes of data.
      * */
-    unsigned short getByteCount() const {return m_byte_count;}
+    unsigned short getByteCount() const { return m_byte_count; }
 
     /**
-     * @brief Fills payload pointer as full as possible (i.e. at most RADIO_MAX_SUBPACKET_SIZE bytes)
+     * @brief Fills payload pointer as full as possible (i.e. at most
+     * RADIO_MAX_SUBPACKET_SIZE bytes)
      * @param payload Pointer to payload array.
      * @return Size of payload data filled in.
      */
     uint8_t fillPayload(uint8_t *payload);
 
-private:
-    std::deque<SubPktPtr> m_subpacket_q; //subpacket_queue, but the ueue is silent
+  private:
+    std::deque<SubPktPtr>
+        m_subpacket_q; // subpacket_queue, but the ueue is silent
     unsigned short m_byte_count = 0;
     unsigned short const M_MAX_BYTES;
 
     /**
-     * @brief Dump entire queue into payload. Must check that payload is large enough and queue is small enough that this works (i.e. m_byte_count < payload array size); otherwise this is undefined behaviour
-     * @return Size of payload data filled in (= m_byte_count before this starts).
+     * @brief Dump entire queue into payload. Must check that payload is large
+     * enough and queue is small enough that this works (i.e. m_byte_count <
+     * payload array size); otherwise this is undefined behaviour
+     * @return Size of payload data filled in (= m_byte_count before this
+     * starts).
      */
     uint8_t dumpAllIntoPayload(uint8_t *payload);
 
     /**
-     * @brief Dispenses at most RADIO_MAX_SUBPACKET_SIZE (255) bytes worth of subpackets into payload.
+     * @brief Dispenses at most RADIO_MAX_SUBPACKET_SIZE (255) bytes worth of
+     * subpackets into payload.
      * @return Size of payload data filled in.
      */
     uint8_t dispenseIntoPayload(uint8_t *payload);
--- avionics/envs/x86/src/main.cpp
+++ avionics/envs/x86/src/main.cpp
@@ -1,7 +1,57 @@
-#include "env_config.h"
+
+// barometer data file
+#include <string>
+const std::string BAROMETER_DATA{"data/barometer.csv"};
+
+#include "HAL/port_impl.h"
+#include "stdio_controller.hpp"
+StdIoController std_io_controller;
+std::mutex StdIoController::s_cout;
+
+auto static SerialUSB = Hal::NativeSerial(1, std_io_controller);
+auto static SerialGPS = Hal::NativeSerial(2, std_io_controller);
+auto static SerialRadio = Hal::NativeSerial(3, std_io_controller);
+auto static IridiumSerial = Hal::NativeSerial(4, std_io_controller);
+auto static SerialCamera = Hal::NativeSerial(5, std_io_controller);
+
+#include "CSVwrite.h"
+class NativeDumbCSVImpl { // TODO - make this not full of no-ops
+  public:
+    bool init(char const *filename) { return true; }
+
+    /**
+     * @brief writes t to the next csv column in order
+     * @param t the data to write
+     */
+    template <typename T> void print(T t) {}
+
+    /**
+     * @brief writes t and ends the line
+     * @param t the data to write
+     */
+    template <typename T> void println(T t) {}
+
+    /**
+     * @brief flushes the cached data to the SD card
+     */
+    void flush() {}
+};
+
+constexpr char LOG_FILE_NAME[] = "datalog.csv";
+CSVWrite<NativeDumbCSVImpl> datalog;
+
+/* Buzzer */
+#include "buzzer.h"
+static NativeBuzzer buzzer;
+
+void setup(void){};
+void loop(void){};
+
+#include "radio.h"
 
 int main(void) {
     setup();
+    NativeRadioController radio(800, std_io_controller);
     for (;;) {
         loop();
     }
