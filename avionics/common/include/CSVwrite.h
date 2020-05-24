ff4cb13921b84b74f06b4b6baa3637a02e03d691
Jesse Li <jesse.li2002@gmail.com>
2020-05-24 13:38:43 -0700

    Formatted CSV write

--- avionics/common/include/CSVwrite.h
+++ avionics/common/include/CSVwrite.h
@@ -1,43 +1,40 @@
-#ifndef CSVWRITE_H_53A4CAED991D46F687680132192B5E5A
-#define CSVWRITE_H_53A4CAED991D46F687680132192B5E5A
+#pragma once
 
 /**
-  * CSV writing Class
-  * Wrapper for SD.h which makes printing to csv easier
-  */
+ * CSV writing Class
+ * Wrapper for SD.h which makes printing to csv easier
+ */
 
 /*Includes------------------------------------------------------------*/
 
-#include "sensors-interface.h"  // ISensor
-#include "state_interface.h"    // StateId
+#include "sensors-interface.h" // ISensor
+#include "state_interface.h"   // StateId
 
 /*
 Requirements on Impl:
--Has init method that initializes the implementation. takes a char* (filename) and returns boolean (true if success, false if failure)
--Has templated print and println functions that take in values to be printed. println should do the same thing as print, except put a newline character at the end.
--Has void flush(void) method that flushes the buffer.
+-Has init method that initializes the implementation. takes a char* (filename)
+and returns boolean (true if success, false if failure) -Has templated print and
+println functions that take in values to be printed. println should do the same
+thing as print, except put a newline character at the end. -Has void flush(void)
+method that flushes the buffer.
 */
 
 // TODO: Add Impl class or integrate in this class.
 // TODO: Figure out how to interface with SD Card
 
-template <typename Impl>
-class CSVWrite {
-    public:
+template <typename Impl> class CSVWrite {
+  public:
     /**
      * @brief initializes the SD card connection
      * @return false if the sensor fails to initialize
      */
-    bool init(const char* filename) {
-        return m_datalog.init(filename);
-    }
+    bool init(const char *filename) { return m_datalog.init(filename); }
 
     /**
      * @brief prints t to the next csv column in order
      * @param s the data in string to print
      */
-    template<typename T>
-    void print(T s) {
+    template <typename T> void print(T s) {
         m_datalog.print(s);
         m_datalog.print(",");
     }
@@ -46,17 +43,12 @@ class CSVWrite {
      * @brief prints s and ends the line
      * @param s the in string data to print
      */
-    template<typename T>
-    void println(T s) {
-        m_datalog.println(s);
-    }
+    template <typename T> void println(T s) { m_datalog.println(s); }
 
     /**
      * @brief flushes the cached data to the SD card
      */
-    void flush() {
-        m_datalog.flush();
-    }
+    void flush() { m_datalog.flush(); }
 
     /**
      * @brief Logs data on the SD card
@@ -66,24 +58,17 @@ class CSVWrite {
      * @param altitude Calculated rocket altitude, after filtering
      * @param baseline_pressure Pressure used as "ground level"
      */
-    void logData(unsigned long timestamp, SensorSet &sensors,
-                 StateId state, float altitude, float baseline_pressure) {
+    void logData(unsigned long timestamp, SensorSet &sensors, StateId state,
+                 float altitude, float baseline_pressure) {
 
         /*
          * SD Card data layout:
-         * Timestamp, Rocket flight state, Altitude, Baseline Pressure, Sensor Data
+         * Timestamp, Rocket flight state, Altitude, Baseline Pressure, Sensor
+         * Data
          */
-
-        // Print Timestamp
         print(timestamp);
-        
-        // Print Rocket flight state
         printState(state);
-
-        // Print Altitude
         print(altitude);
-
-        // Print Baseline Pressure
         print(baseline_pressure);
 
         // Print Sensor Data
@@ -97,50 +82,38 @@ class CSVWrite {
         flush();
     }
 
-    private:
+  private:
     /**
      * @brief Prints out current flight state
      * @param state Flight state enum class
      */
     void printState(StateId state) {
-        if(state == StateId::STANDBY) {
+        if (state == StateId::STANDBY) {
             print("Standby");
-        }
-        else if(state == StateId::ARMED) {
+        } else if (state == StateId::ARMED) {
             print("Armed");
-        }
-        else if(state == StateId::POWERED_ASCENT) {
+        } else if (state == StateId::POWERED_ASCENT) {
             print("Powered_Ascent");
-        }
-        else if(state == StateId::PRE_AIR_START_COAST_TIMED) {
+        } else if (state == StateId::PRE_AIR_START_COAST_TIMED) {
             print("Pre_Air_Start_Coast_Timed");
-        }
-        else if(state == StateId::ASCENT_TO_APOGEE) {
+        } else if (state == StateId::ASCENT_TO_APOGEE) {
             print("Ascent_To_Apogee");
-        }
-        else if(state == StateId::MACH_LOCK) {
+        } else if (state == StateId::MACH_LOCK) {
             print("Mach_Lock");
-        }
-        else if(state == StateId::PRESSURE_DELAY) {
+        } else if (state == StateId::PRESSURE_DELAY) {
             print("Pressure_Delay");
-        }
-        else if(state == StateId::DROGUE_DESCENT) {
+        } else if (state == StateId::DROGUE_DESCENT) {
             print("Drogue_Descent");
-        }
-        else if(state == StateId::MAIN_DESCENT) {
+        } else if (state == StateId::MAIN_DESCENT) {
             print("Main_Descent");
-        }
-        else if(state == StateId::LANDED) {
+        } else if (state == StateId::LANDED) {
             print("Landed");
-        }
-        else if(state == StateId::WINTER_CONTINGENCY) {
+        } else if (state == StateId::WINTER_CONTINGENCY) {
             print("Winter_Contingency");
-        }
-        else {
+        } else {
             print("Error_Unknown_State");
         }
     }
     // File m_datalog;
     Impl m_datalog;
 };
-#endif
