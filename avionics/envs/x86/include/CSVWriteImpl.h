8448f4f2478402b672b80a15e71bb98938512e51
Jesse Li <jesse.li2002@gmail.com>
2020-06-28 23:27:57 -0700

    Implementation

--- avionics/envs/x86/include/CSVWriteImpl.h
+++ avionics/envs/x86/include/CSVWriteImpl.h
@@ -1,23 +1,39 @@
 #pragma once
 
-class CSVWriteImpl { // TODO - make this not full of no-ops
+#include <fstream>
+#include <iostream>
+
+class CSVWriteImpl {
   public:
-    bool init(char const *filename) { return true; }
+    ~CSVWriteImpl() { file_.close(); }
+
+    /**
+     * @brief Delayed constructor.
+     * @param filename Name of the file to open.
+     * @return True on successful file opening.
+     */
+    bool init(char const *filename) {
+        file_.open(filename);
+        return static_cast<bool>(file_);
+    }
 
     /**
      * @brief writes t to the next csv column in order
      * @param t the data to write
      */
-    template <typename T> void print(T t) {}
+    template <typename T> void print(T t) { file_ << t; }
 
     /**
      * @brief writes t and ends the line
      * @param t the data to write
      */
-    template <typename T> void println(T t) {}
+    template <typename T> void println(T t) { file_ << t << "\n"; }
 
     /**
      * @brief flushes the cached data to the SD card
      */
-    void flush() {}
+    void flush() { file_.flush(); }
+
+  private:
+    std::ofstream file_;
 };
