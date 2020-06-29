c38013c1f30d5b29cc510bf26a530037b5034439
Jesse Li <jesse.li2002@gmail.com>
2020-06-28 19:51:27 -0700

    Privitized some unnecessarily public CSVWrite methods

--- avionics/common/include/CSVwrite.h
+++ avionics/common/include/CSVwrite.h
@@ -31,21 +31,6 @@ template <typename Impl> class CSVWrite {
     bool init(const char *filename) { return m_datalog.init(filename); }
 
     /**
-     * @brief prints t to the next csv column in order
-     * @param s the data in string to print
-     */
-    template <typename T> void print(T s) {
-        m_datalog.print(s);
-        m_datalog.print(",");
-    }
-
-    /**
-     * @brief prints s and ends the line
-     * @param s the in string data to print
-     */
-    template <typename T> void println(T s) { m_datalog.println(s); }
-
-    /**
      * @brief flushes the cached data to the SD card
      */
     void flush() { m_datalog.flush(); }
@@ -67,7 +52,7 @@ template <typename Impl> class CSVWrite {
          * Data
          */
         print(timestamp);
-        printState(state);
+        print(state);
         print(altitude);
         print(baseline_pressure);
 
@@ -81,49 +66,65 @@ template <typename Impl> class CSVWrite {
 
   private:
     /**
-     * @brief Prints out current flight state
-     * @param state Flight state enum class
+     * @brief prints t to the next csv column in order
+     * @param s the data in string to print
      */
-    void printState(StateId state) {
-        switch(state) {
-            case StateId::STANDBY:
-                print("Standby");
-                break;
-            case StateId::ARMED:
-                print("Armed");
-                break;
-            case StateId::POWERED_ASCENT:
-                print("Powered_Ascent");
-                break;
-            case StateId::PRE_AIR_START_COAST_TIMED:
-                print("Pre_Air_Start_Coast_Timed");
-                break;
-            case StateId::ASCENT_TO_APOGEE:
-                print("Ascent_To_Apogee");
-                break;
-            case StateId::MACH_LOCK:
-                print("Mach_Lock");
-                break;
-            case StateId::PRESSURE_DELAY:
-                print("Pressure_Delay");
-                break;
-            case StateId::DROGUE_DESCENT:
-                print("Drogue_Descent");
-                break;
-            case StateId::MAIN_DESCENT:
-                print("Main_Descent");
-                break;
-            case StateId::LANDED:
-                print("Landed");
-                break;
-            case StateId::WINTER_CONTINGENCY:
-                print("Winter_Contingency");
-                break;
-            default:
-                print("Error_Unknown_State");
-                break;
+    template <typename T> void print(T s) {
+        m_datalog.print(s);
+        m_datalog.print(",");
+    }
+
+    /**
+     * @brief prints s and ends the line
+     * @param s the in string data to print
+     */
+    template <typename T> void println(T s) { m_datalog.println(s); }
+
+    /**
+     * @brief Specialization of print() for StateId.
+     * @param state The state to print
+     */
+    void print(StateId state) {
+        switch (state) {
+        case StateId::STANDBY:
+            print("Standby");
+            break;
+        case StateId::ARMED:
+            print("Armed");
+            break;
+        case StateId::POWERED_ASCENT:
+            print("Powered_Ascent");
+            break;
+        case StateId::PRE_AIR_START_COAST_TIMED:
+            print("Pre_Air_Start_Coast_Timed");
+            break;
+        case StateId::ASCENT_TO_APOGEE:
+            print("Ascent_To_Apogee");
+            break;
+        case StateId::MACH_LOCK:
+            print("Mach_Lock");
+            break;
+        case StateId::PRESSURE_DELAY:
+            print("Pressure_Delay");
+            break;
+        case StateId::DROGUE_DESCENT:
+            print("Drogue_Descent");
+            break;
+        case StateId::MAIN_DESCENT:
+            print("Main_Descent");
+            break;
+        case StateId::LANDED:
+            print("Landed");
+            break;
+        case StateId::WINTER_CONTINGENCY:
+            print("Winter_Contingency");
+            break;
+        default:
+            print("Error_Unknown_State");
+            break;
         }
     }
+
     // File m_datalog;
     Impl m_datalog;
 };
