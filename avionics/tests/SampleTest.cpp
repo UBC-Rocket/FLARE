ddf444845b6066d32561e434e65fbfbd88ad9118
Harry Tung <honeytung@gmail.com>
2020-08-18 02:12:40 +0800

    Added Instructions for SampleTest.cpp

--- avionics/tests/SampleTest.cpp
+++ avionics/tests/SampleTest.cpp
@@ -1,6 +1,18 @@
 #include "gtest/gtest.h"
 
-TEST(SubtractTest, SubtractTwoNumbers)
+/*
+ * Sample Test Template
+ * 
+ * Add Tests: 
+ *      For adding new tests, you will also need to link those tests in CMakeLists.txt.
+ *
+ * Run Tests:
+ *      Build project in build folder, then run "make test"
+ */
+
+TEST(SampleTest, SampleTestTemplate)
 {
-    EXPECT_EQ(5, 1);
+    EXPECT_EQ(2, 2);
+    ASSERT_EQ(3, 3);
+    ASSERT_TRUE(true);
 }
