a9d5f230c2af4a435090d4766aac4ec9eccf589a
Jesse Li <jesse.li2002@gmail.com>
2020-12-25 14:23:37 -0800

    Clean up config.h

--- avionics/rockets/tantalus_stage_1/include/config.h
+++ avionics/rockets/tantalus_stage_1/include/config.h
@@ -1,17 +1,1 @@
 #pragma once
-
-/* Sensors */
-#include "sensors/GPS.h"
-#include "sensors/IMU.h"
-#include "sensors/accelerometer.h"
-#include "sensors/barometer.h"
-#include "sensors/temperature.h"
-#include "sensors_interface.h"
-
-namespace SensorPositions {
-constexpr uint8_t BAROMETER = 0;
-constexpr uint8_t GPS = 1;
-constexpr uint8_t ACCELEROMETER = 2;
-constexpr uint8_t IMU = 3;
-constexpr uint8_t TEMPERATURE = 4;
-} // namespace SensorPositions
