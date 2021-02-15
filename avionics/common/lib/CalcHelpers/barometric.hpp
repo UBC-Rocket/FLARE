b85dbb61379d4239aacee3d7071895a075444de8
Jesse Li <jesse.li2002@gmail.com>
2021-02-14 19:02:37 -0800

    Pressure to altitude test

--- avionics/common/lib/CalcHelpers/barometric.hpp
+++ avionics/common/lib/CalcHelpers/barometric.hpp
@@ -1,4 +1,8 @@
 #pragma once
 #include <cmath>
 
+/**
+ * @brief Returns the altitude above sea-level given pressure.
+ * See https://en.wikipedia.org/wiki/Barometric_formula
+*/
 float pressureToAltitude(float const pres);
