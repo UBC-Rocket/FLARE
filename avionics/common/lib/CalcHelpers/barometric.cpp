50397418166bd02f78a818db8f4a477c6222aa4f
Andrei Popescu <Popescu.Andrei.David@gmail.com>
2021-02-17 17:56:31 -0800

    Fixed incorrect pressure to altitude calculations

--- avionics/common/lib/CalcHelpers/barometric.cpp
+++ avionics/common/lib/CalcHelpers/barometric.cpp
@@ -1,12 +1,14 @@
 #include "CalcHelpers/barometric.hpp"
 
-float pressureToAltitude(float const pres) {
+float /* m */ pressureToAltitude(float pres /* mbar */) {
     constexpr float Pb = 101325;
     constexpr float Tb = 288.15;
     constexpr float Lb = -0.0065;
     constexpr float R = 8.3144598;
     constexpr float g0 = 9.80665;
     constexpr float M = 0.0289644;
+
+    pres *= 100; // mbar to Pa
 
     return Tb / Lb * (std::pow(Pb / pres, R * Lb / (g0 * M)) - 1);
 }
