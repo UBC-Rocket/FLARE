53ea655eccdb32f83e53e1bcadfbc34f0fc8329a
Andrei Popescu <Popescu.Andrei.David@gmail.com>
2021-02-20 20:30:13 -0800

    Revert "Fixed incorrect pressure to altitude calculations"
    This reverts commit 50397418166bd02f78a818db8f4a477c6222aa4f.

--- avionics/common/lib/CalcHelpers/barometric.cpp
+++ avionics/common/lib/CalcHelpers/barometric.cpp
@@ -6,15 +6,13 @@
  * @param pascals Pressure, in [Pa]
  * @return Altitude above sea level [m]
  */
-float /* m */ pressureToAltitude(float pres /* mbar */) {
+float pressureToAltitude(float const pres) {
     constexpr float Pb = 101325;
     constexpr float Tb = 288.15;
     constexpr float Lb = -0.0065;
     constexpr float R = 8.3144598;
     constexpr float g0 = 9.80665;
     constexpr float M = 0.0289644;
-
-    pres *= 100; // mbar to Pa
 
     return Tb / Lb * (std::pow(Pb / pres, R * Lb / (g0 * M)) - 1);
 }
