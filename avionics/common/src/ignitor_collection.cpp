9eeb399889d8addf4de89d989d56fe37d35608ed
Jesse Li <jesse.li2002@gmail.com>
2021-05-23 14:20:50 -0700

    Miscellaneous additional logs

--- avionics/common/src/ignitor_collection.cpp
+++ avionics/common/src/ignitor_collection.cpp
@@ -1,5 +1,6 @@
 #include "ignitor_collection.h"
 #include "gpio.h"
+#include "log.hpp"
 
 IgnitorCollection::IgnitorCollection()
     : drogue(Pin::DROGUE_IGNITOR, Pin::DROGUE_CONTINUITY_TEST,
@@ -7,11 +8,14 @@ IgnitorCollection::IgnitorCollection()
       main(Pin::MAIN_IGNITOR, Pin::MAIN_CONTINUITY_TEST,
            Pin::MAIN_CONTINUITY_READ) {
     status_ = RocketStatus::NOMINAL;
+
     if (main.getStatus() == ComponentStatus::FAILURE) {
+        LOG_ERROR("Broken ignitor for main parachute");
         status_bitfield_[0] |= 0x80;
         status_ = RocketStatus::CRITICAL_FAILURE;
     }
     if (drogue.getStatus() == ComponentStatus::FAILURE) {
+        LOG_ERROR("Broken ignitor for drogue parachute");
         status_bitfield_[0] |= 0x40;
         status_ = RocketStatus::CRITICAL_FAILURE;
     }
--- avionics/common/src/sensor_collection.cpp
+++ avionics/common/src/sensor_collection.cpp
@@ -1,4 +1,5 @@
 #include "sensor_collection.h"
+#include "log.hpp"
 
 constexpr char SensorCollection::LOG_FILE_HEADER[];
 
@@ -30,23 +31,28 @@ void SensorCollection::updateStatus() {
     status_ = RocketStatus::NOMINAL;
     status_bitfield_[0] = 0;
     if (barometer.getStatus() == SensorStatus::FAILURE) {
+        LOG_ERROR("Barometer failed");
         raiseToStatus(status_, RocketStatus::CRITICAL_FAILURE);
         *status_bitfield_ |= 0x80;
     }
 
     if (gps.getStatus() == SensorStatus::FAILURE) {
+        LOG_WARN("GPS failed");
         raiseToStatus(status_, RocketStatus::NONCRITICAL_FAILURE);
         *status_bitfield_ |= 0x40;
     }
     if (accelerometer.getStatus() == SensorStatus::FAILURE) {
+        LOG_WARN("Accelerometer failed");
         raiseToStatus(status_, RocketStatus::NONCRITICAL_FAILURE);
         *status_bitfield_ |= 0x20;
     }
     if (imuSensor.getStatus() == SensorStatus::FAILURE) {
+        LOG_WARN("IMU failed");
         raiseToStatus(status_, RocketStatus::NONCRITICAL_FAILURE);
         *status_bitfield_ |= 0x10;
     }
     if (temperature.getStatus() == SensorStatus::FAILURE) {
+        LOG_WARN("Temperature sensor failed");
         raiseToStatus(status_, RocketStatus::NONCRITICAL_FAILURE);
         *status_bitfield_ |= 0x08;
     }
