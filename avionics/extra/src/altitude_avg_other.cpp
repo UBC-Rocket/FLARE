97474751e1eea6db665f3e7586fe2445b047f3f2
jordans6 <schneiderjord@aquinas.org>
2021-09-23 19:57:39 -0700

    Add calc subclass that overrides update() method for other states

---
+++ avionics/extra/src/altitude_avg_other.cpp
@@ -0,0 +1,27 @@
+#include "extra/altitude_avg_other.hpp"
+
+namespace extra {
+namespace estimator {
+
+class AltitudeAvgOther : public AltitudeAvg {
+    
+    public void update(Hal::t_point t_ms) override {
+        const float raw_alt = rawAltitude();
+    
+        // Update AGL altitude
+        const float raw_agl_alt = raw_alt - base_alt_.getAverage();
+        agl_alt_.addValue(raw_agl_alt);
+
+        // Update velocity
+        constexpr int MILLISECONDS_PER_SECOND = 1000;
+        velocity_z_ = (altitude() - last_agl_alt_) / (t_ms - last_t_).count() *
+                  MILLISECONDS_PER_SECOND;
+
+        // Update last iteration info
+        last_agl_alt_ = altitude();
+        last_t_ = t_ms;
+    }   
+}
+
+} // namespace estimator
+} // namespace extra
