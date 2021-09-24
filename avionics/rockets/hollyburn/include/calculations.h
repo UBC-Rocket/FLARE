58e9caeca4a1dd53b11f7c14822624e96e4c63c5
jordans6 <schneiderjord@aquinas.org>
2021-09-23 19:59:17 -0700

    Add typedefs for the new Calculator subclasses

--- avionics/rockets/hollyburn/include/calculations.h
+++ avionics/rockets/hollyburn/include/calculations.h
@@ -1,5 +1,9 @@
 #pragma once
 
 #include "extra/altitude_avg.hpp"
+#include "extra/altitude_avg_other.hpp"
+#include "extra/altitude_avg_standby_armed.hpp"
 
 typedef extra::estimator::AltitudeAvg Calculator;
+typedef extra::estimator::AltitudeAvgOther CalculatorOther;
+typedef extra::estimator::AltitudeAvgStandbyArmed CalculatorStandbyArmed;
