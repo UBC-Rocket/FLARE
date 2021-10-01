32dbc1e740fbaf8dc001dbb5ec2279b3dbfebb8f
jordans6 <schneiderjord@aquinas.org>
2021-10-01 15:28:57 -0700

    Adjust baseclass implementation

--- avionics/extra/src/altitude_avg_other.cpp
+++ avionics/extra/src/altitude_avg_other.cpp
@@ -3,27 +3,25 @@
 namespace extra {
 namespace estimator {
 
-class AltitudeAvgOther : public AltitudeAvg {
-
-    public AltitudeAvgOther::AltitudeAvgOther(SensorCollection &sensors, Hal::t_point initial_time) : AltitudeAvg(sensors, initial_time) {};
+public AltitudeAvgOther::AltitudeAvgOther(SensorCollection &sensors, Hal::t_point initial_time) : AltitudeAvg(sensors, initial_time) {};
     
-    public void update(Hal::t_point t_ms) override {
-        const float raw_alt = rawAltitude();
+public void update(Hal::t_point t_ms) override {
+    const float raw_alt = rawAltitude();
     
-        // Update AGL altitude
-        const float raw_agl_alt = raw_alt - base_alt_.getAverage();
-        agl_alt_.addValue(raw_agl_alt);
+    // Update AGL altitude
+    const float raw_agl_alt = raw_alt - base_alt_.getAverage();
+    agl_alt_.addValue(raw_agl_alt);
 
-        // Update velocity
-        constexpr int MILLISECONDS_PER_SECOND = 1000;
-        velocity_z_ = (altitude() - last_agl_alt_) / (t_ms - last_t_).count() *
-                  MILLISECONDS_PER_SECOND;
+    // Update velocity
+    constexpr int MILLISECONDS_PER_SECOND = 1000;
+    velocity_z_ = (altitude() - last_agl_alt_) / (t_ms - last_t_).count() *
+                MILLISECONDS_PER_SECOND;
 
-        // Update last iteration info
-        last_agl_alt_ = altitude();
-        last_t_ = t_ms;
-    }   
-}
+    // Update last iteration info
+    last_agl_alt_ = altitude();
+    last_t_ = t_ms;
+}   
+
 
 } // namespace estimator
 } // namespace extra
--- avionics/extra/src/altitude_avg_standby_armed.cpp
+++ avionics/extra/src/altitude_avg_standby_armed.cpp
@@ -3,31 +3,30 @@
 namespace extra {
 namespace estimator {
 
-class AltitudeAvgStandbyArmed : public AltitudeAvg {
 
-    public AltitudeAvgStandbyArmed::AltitudeAvgStandbyArmed(SensorCollection &sensors, Hal::t_point initial_time) : AltitudeAvg(sensors, initial_time) {};
+public AltitudeAvgStandbyArmed::AltitudeAvgStandbyArmed(SensorCollection &sensors, Hal::t_point initial_time) : AltitudeAvg(sensors, initial_time) {};
     
-    public void update(Hal::t_point t_ms) override {
-        const float raw_alt = rawAltitude();
+public void update(Hal::t_point t_ms) override {
+    const float raw_alt = rawAltitude();
     
-        const float diff = std::abs(base_alt_.getAverage() - raw_alt);
-        if (diff < base_alt_.getStandardDeviation() * 3) {
-            base_alt_.addValue(raw_alt);
-        }
+    const float diff = std::abs(base_alt_.getAverage() - raw_alt);
+    if (diff < base_alt_.getStandardDeviation() * 3) {
+        base_alt_.addValue(raw_alt);
+    }
     
-        // Update AGL altitude
-        const float raw_agl_alt = raw_alt - base_alt_.getAverage();
-        agl_alt_.addValue(raw_agl_alt);
+    // Update AGL altitude
+    const float raw_agl_alt = raw_alt - base_alt_.getAverage();
+    agl_alt_.addValue(raw_agl_alt);
 
-        // Update velocity
-        constexpr int MILLISECONDS_PER_SECOND = 1000;
-        velocity_z_ = (altitude() - last_agl_alt_) / (t_ms - last_t_).count() *
-                  MILLISECONDS_PER_SECOND;
+    // Update velocity
+    constexpr int MILLISECONDS_PER_SECOND = 1000;
+    velocity_z_ = (altitude() - last_agl_alt_) / (t_ms - last_t_).count() *
+                MILLISECONDS_PER_SECOND;
 
-        // Update last iteration info
-        last_agl_alt_ = altitude();
-        last_t_ = t_ms;
-    }   
+    // Update last iteration info
+    last_agl_alt_ = altitude();
+    last_t_ = t_ms;
+     
 }
 
 } // namespace estimator
