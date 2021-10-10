49d4cc7ca275a215448bc4bb1c5c96947a979e41
jordans6 <schneiderjord@aquinas.org>
2021-10-10 16:59:14 -0700

    Minor Fixes

--- avionics/extra/extra/altitude_avg_other.hpp
+++ avionics/extra/extra/altitude_avg_other.hpp
@@ -4,9 +4,10 @@ namespace extra {
 namespace estimator {
 
 class AltitudeAvgOther : public AltitudeAvg {
-    public AltitudeAvgOther(SensorCollection &sensors, Hal::t_point initial_time) : AltitudeAvg(sensors, initial_time);
+    public: 
+        AltitudeAvgOther(SensorCollection &sensors, Hal::t_point initial_time) : AltitudeAvg(sensors, initial_time);
 
-    public void update(Hal::t_point t_ms) override;
+        void update(Hal::t_point t_ms) override;
 };
 
 } // namespace estimator
--- avionics/extra/extra/altitude_avg_standby_armed.hpp
+++ avionics/extra/extra/altitude_avg_standby_armed.hpp
@@ -4,9 +4,10 @@ namespace extra {
 namespace estimator {
 
 class AltitudeAvgStandbyArmed : public AltitudeAvg {
-    public AltitudeAvgStandbyArmed(SensorCollection &sensors, Hal::t_point initial_time) : AltitudeAvg(sensors, initial_time);
+    public:
+        AltitudeAvgStandbyArmed(SensorCollection &sensors, Hal::t_point initial_time) : AltitudeAvg(sensors, initial_time);
 
-    public void update(Hal::t_point t_ms) override;
+        void update(Hal::t_point t_ms) override;
 };
 
 } // namespace estimator
