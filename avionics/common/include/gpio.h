62e8cecc59d16ca50b5ef69796a483ddf6e27c8f
Marco Yuen <yuenmarco17@gmail.com>
2023-04-30 11:07:17 -0700

    update some pins and create pio target for f411 board

--- avionics/common/include/gpio.h
+++ avionics/common/include/gpio.h
@@ -20,24 +20,23 @@
  */
 
 #pragma once
-#include "variant_NUCLEO_L476RG.h"
 
 /*Constants------------------------------------------------------------*/
 // Double, triple check pin assignments
 enum class Pin {
-    POWER_LED = 20,  // kept in one place w/ FLIGHT_LED
-    FLIGHT_LED = 21, // required by state machine
-    BUILTIN_LED = 205, // PA5
-    STATUS_LED = 205, // uses BUILTIN_LED
-    BUZZER = 3,      // required by buzzer
-    MAIN_IGNITOR = 16,
-    MAIN_CONTINUITY_TEST = 4,
-    MAIN_CONTINUITY_READ = 14,
-    DROGUE_IGNITOR = 35,
-    DROGUE_CONTINUITY_TEST = 17,
-    DROGUE_CONTINUITY_READ = 34,
-    VOLTAGE_SENSOR = 23,
-    SD_CHIP_SELECT = PA9,
+    POWER_LED = 33,  // kept in one place w/ FLIGHT_LED
+    FLIGHT_LED = 33, // required by state machine
+    BUILTIN_LED = 31,
+    STATUS_LED = 31, // uses BUILTIN_LED
+    BUZZER = 25,      // required by buzzer
+    MAIN_IGNITOR = 15,
+    MAIN_CONTINUITY_TEST = 33,
+    MAIN_CONTINUITY_READ = 33,
+    DROGUE_IGNITOR = 20,
+    DROGUE_CONTINUITY_TEST = 33,
+    DROGUE_CONTINUITY_READ = 33,
+    VOLTAGE_SENSOR = 33, // PC15 (unused)
+    SD_CHIP_SELECT = 196, // PA4
 };
 
 /*Functions------------------------------------------------------------*/
