f38a40b017b9a0f8ecbe3d1297a58a3b18e51e05
Marco Yuen <yuenmarco17@gmail.com>
2023-06-13 10:45:38 -0700

    buzz top gun on nominal startup

--- avionics/common/src/avionics.cpp
+++ avionics/common/src/avionics.cpp
@@ -79,7 +79,7 @@ PLEASE READ ME!
 
 #include "log.hpp"
 #include "HAL/pin_util.h"
-#include "variant_NUCLEO_L476RG.h"
+#include "variant_BLACKPILL_F411CE.h"
 
 /**
  * @brief Helper function that makes things less verbose; basically saves the
@@ -88,6 +88,12 @@ PLEASE READ ME!
 void registerTask(TaskID id, Scheduler::Task task, bool repeat = true,
                   bool early = false) {
     Scheduler::registerTask(static_cast<int>(id), task, repeat, early);
+}
+
+// TODO: Move to buzzer.cpp
+void buzzNote(int frequency, int length = 750, int pause = 50) {
+    tone(static_cast<uint8_t>(Pin::BUZZER), frequency, length);
+    Hal::sleep_ms(length + pause);
 }
 
 /* Main function ====================================================== */
@@ -106,30 +112,59 @@ int main(void) {
 #ifdef TESTING
     Serial.begin(9600);
     while (!Serial) {
+        Hal::digitalWrite(Pin::BUILTIN_LED, Hal::PinDigital::HI);
+        Hal::sleep_ms(100);
+        Hal::digitalWrite(Pin::BUILTIN_LED, Hal::PinDigital::LO);
+        Hal::sleep_ms(100);
     }
     Serial.println("Initializing...");
-    Serial.println(PA5);
-    Serial.println(PA5 == 205);
+    
+    // Serial.print("PA9 "); Serial.println(PA9); Serial.println(PA9 == PA_9);
+    // Serial.print("PA10 "); Serial.println(PA10); Serial.println(PA10 == PA_10);
+    // Serial.print("PA2 "); Serial.println(PA2); Serial.print("PA_2: "); Serial.println(PA_2); Serial.println(PA2 == PA_2);
+    // Serial.print("PA3 "); Serial.println(PA3); Serial.print("PA_3: "); Serial.println(PA_3); Serial.println(PA3 == PA_3);
+    // Serial.print("PA5"); Serial.println(PA5);
+    // Serial.println(PA5 == 205);
+    // Serial.print("PA6: "); Serial.println(static_cast<uint8_t>(Pin::DROGUE_CONTINUITY_READ_));
+    // Serial.print("PA7: "); Serial.println(static_cast<uint8_t>(Pin::IGNITOR_3_READ_));
+    // Serial.print("PC0: "); Serial.println(PC0);
+    // Serial.print("PC1: "); Serial.println(PC1);
+    // Serial.print("PA15: "); Serial.println(PA15);
+    // Serial.print("PA_15: "); Serial.println(PA_15);
+    // Serial.print("PB4: "); Serial.println(PB4);
+    // Serial.print("PB_4: "); Serial.println(PB_4);
+    // Serial.print("PA0: "); Serial.println(PA0);
+    // Serial.print("PA_0: "); Serial.println(PA_0);
+    // Serial.print("PA1: "); Serial.println(PA1);
+    // Serial.print("PA_1: "); Serial.println(PA_1);
+    // Serial.print("PB0: "); Serial.println(PB0);
+    // Serial.print("PB_0: "); Serial.println(PB_0);
+
 #endif
 
     Radio::initialize();
+    // Serial.println("initialized radio");
     LOG_INFO("Initialized radio");
     Rocket rocket;
+    // Serial.println("initialized rocket");
     // Logically, these are all unrelated variables - but to allow the command
     // receiver to function, they need to be coalesced into one POD struct.
     auto &state_machine = rocket.state_machine;
     auto &init_status = rocket.init_status;
     auto &sensors = rocket.sensors;
     auto &ignitors = rocket.ignitors;
+    // Serial.println("destructure rocket stuff");
 
     if (init_status == RocketStatus::CRITICAL_FAILURE) {
-        Serial.println("abort");
+        // Serial.println("abort");
         LOG_ERROR("Critical failure; aborting in state machine");
         state_machine.abort();
     }
 
+// Serial.println("send startup status");
     Radio::sendStatus(Hal::millis(), init_status, sensors, ignitors);
     LOG_INFO("Sent startup status");
+    // Serial.println("finish send startup status");
 
     /* Register all tasks */
     typedef Scheduler::Task Task;
@@ -150,16 +185,53 @@ int main(void) {
 
     switch (init_status) {
     case RocketStatus::NOMINAL:
+    {
     #ifdef TESTING
         Serial.println("Nominal");
     #endif
         Hal::digitalWrite(Pin::BUILTIN_LED, Hal::PinDigital::LO);
+
+        int A = 440;
+        int Bf = 466;
+        int B = 494;
+        int C = 523;
+        int Cs = 554;
+        int D = 587;
+        int Ds = 622;
+        int E = 659;
+        int F = 698;
+        int Fs = 740;
+        int G = 784;
+
+    #ifdef STAGE_2
+        buzzNote(C, 500);
+        buzzNote(G, 500);
+        buzzNote(G, 500);
+        buzzNote(F, 250);
+        buzzNote(E, 250);
+        buzzNote(F, 250);
+        buzzNote(E, 250);
+        buzzNote(D, 500);
+        buzzNote(D, 500);
+    #else
+        buzzNote(C, 250);
+        buzzNote(D, 250);
+        buzzNote(E, 500);
+        buzzNote(D, 250);
+        buzzNote(E, 250);
+        buzzNote(F, 500);
+        buzzNote(E, 250);
+        buzzNote(C, 250);
+        buzzNote(E, 500);
+        buzzNote(D, 1000);
+    #endif
         break;
+    }
     case RocketStatus::NONCRITICAL_FAILURE:
     #ifdef TESTING
         Serial.println("Non-critical failure");
     #endif
-        registerTask(TaskID::LEDBlinker, led_blink);
+        // registerTask(TaskID::LEDBlinker, led_blink);
         break;
     case RocketStatus::CRITICAL_FAILURE:
     #ifdef TESTING
