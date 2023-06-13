9dac938938f98e275219ef0dd9b927541fa40363
Marco Yuen <yuenmarco17@gmail.com>
2023-06-13 10:48:33 -0700

    update serial pins

--- avionics/envs/board/lib/HAL/port_impl.cpp
+++ avionics/envs/board/lib/HAL/port_impl.cpp
@@ -2,9 +2,10 @@
 
 namespace Hal {
 
-HardwareSerial CustomSerial1(PC_0, PC_1);
-HardwareSerial CustomSerial2(PC_4, PC_5);
-HardwareSerial CustomSerial3(PA_3, PA_2);
+// HardwareSerial CustomSerial1(PC_0, PC_1);
+HardwareSerial CustomSerial1(PA_10, PA_9);
+HardwareSerial CustomSerial2(3, 2);
+HardwareSerial CustomSerial3(PC_15, PC_15);
 
 CustomSerial SerialInst::GPS{CustomSerial1};
 CustomSerial SerialInst::Radio{CustomSerial2};
