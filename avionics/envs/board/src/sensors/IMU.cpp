96e4a5808fe35605661b43a72979a62758e90906
Marco Yuen <yuenmarco17@gmail.com>
2023-07-26 16:25:04 -0700

    clean up IMU reading

--- avionics/envs/board/src/sensors/IMU.cpp
+++ avionics/envs/board/src/sensors/IMU.cpp
@@ -23,40 +23,12 @@ IMU::IMU(float *const data) : SensorBase(data), bmi088{BMI088_ACC_ALT_ADDRESS, B
 }
 
 void IMU::readData() {
-    // #if defined NOSECONE
-    //     sensors_event_t event;
-    //     IMU_Sensor.getEvent(&event);
-    //     data[0] = event.orientation.heading;
-    //     data[1] = event.orientation.roll;
-    //     data[2] = event.orientation.pitch;
-
-    // #elif defined BODY
-    //     imu::Vector<3> accelerometer_data, gyroscope_data, magnetometer_data;
-
-    //     accelerometer_data =
-    //         IMU_Sensor.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
-    //     gyroscope_data =
-    //     IMU_Sensor.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
-    //     magnetometer_data =
-    //         IMU_Sensor.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER);
-
-    //     data[0] = accelerometer_data[0];
-    //     data[1] = accelerometer_data[1];
-    //     data[2] = accelerometer_data[2];
-
-    //     data[3] = gyroscope_data[0];
-    //     data[4] = gyroscope_data[1];
-    //     data[5] = gyroscope_data[2];
-
-    //     data[6] = magnetometer_data[0];
-    //     data[7] = magnetometer_data[1];
-    //     data[8] = magnetometer_data[2];
-    // #endif // NOSECONE elif BODY
-
     bmi088.getAcceleration(&ax, &ay, &az);
     bmi088.getGyroscope(&gx, &gy, &gz);
     imuTemp = bmi088.getTemperature();
+
     status = SensorStatus::NOMINAL;
+    
     data_[0] = ax;
     data_[1] = ay;
     data_[2] = az;
@@ -64,6 +36,7 @@ void IMU::readData() {
     data_[4] = gy;
     data_[5] = gz;
     data_[6] = imuTemp;
+
 #ifdef TESTING
     Serial.println("Polling IMU");
     Serial.print("ax:  ");
